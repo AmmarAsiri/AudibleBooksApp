d40d0a73b9ab332feb43b68dd4ea301a1ea3a9d6
Ammar <aamooor9@gmail.com>
2022-04-03 20:29:41 +0300

    Audible-Books App

---
+++ app/src/main/java/com/example/audiblebooks/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.audiblebooks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        }
+
+    }

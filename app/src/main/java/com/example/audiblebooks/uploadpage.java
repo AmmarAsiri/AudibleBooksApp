dc90643478c42a82a4963a6a4dcea9b881f65311
Ammar <aamooor9@gmail.com>
2022-04-04 14:29:16 +0300

    Audible-Books App

--- app/src/main/java/com/example/audiblebooks/uploadpage.java
+++ app/src/main/java/com/example/audiblebooks/uploadpage.java
@@ -1,38 +1,24 @@
 package com.example.audiblebooks;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.content.ClipData;
-import android.os.Bundle;
-import android.renderscript.Sampler;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.QuickContactBadge;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class homepage extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private ImageView searchBooks;
+import android.os.Bundle;
+
+public class uploadpage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
-
-        searchBooks = findViewById(R.id.searchBooks);
-
-        searchBooks.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_main);
-            }
-        });
-
+        setContentView(R.layout.uploadpage);
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
         bottomNav.setOnNavigationItemSelectedListener(navListner);
     }
@@ -41,8 +27,7 @@ public class homepage extends AppCompatActivity {
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                    switch (item.getItemId()){
+                    switch (item.getItemId()) {
                         case (R.id.menuHome):
                             setContentView(R.layout.homepage);
                             break;
@@ -58,7 +43,14 @@ public class homepage extends AppCompatActivity {
                     }
                     return true;
                 }
-            };
 
+
+            };
 }
+
+
+
+
+
+
 
